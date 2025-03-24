66392f209a2f69a80b04dd918d03a28049e198c9
KhengChamnan <chamnan.kheng@student.cadt.edu.kh>
2025-03-24 21:21:44 +0700

    rideprefprovider

---
+++ lib/ui/providers/ride_prefs_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:week_3_blabla_project/model/ride/ride_pref.dart';
+import 'package:week_3_blabla_project/repository/ride_preferences_repository.dart';
+
+class RidesPreferencesProvider extends ChangeNotifier {
+ RidePreference? _currentPreference;
+ List<RidePreference> _pastPreferences = [];
+ final RidePreferencesRepository repository;
+ RidesPreferencesProvider({required this.repository}) {
+ // For now past preferences are fetched only 1 time
+ // Your code
+  _pastPreferences = repository.getPastPreferences();
+ }
+
+
+ RidePreference? get currentPreference => _currentPreference;
+ 
+
+ //set current pref
+ void setCurrentPreferrence(RidePreference? pref) {
+  if(currentPreference!=pref){
+    _currentPreference=pref;
+  }
+  
+ if(pref!= null){
+    bool isInHistory=_pastPreferences.contains(pref);
+    if(!isInHistory){
+      _pastPreferences.add(pref);
+      //add data to repo
+      _addPreference(pref);
+  }
+ }
+
+ notifyListeners();
+ }
+
+ void _addPreference(RidePreference preference) {
+  return repository.addPreference(preference);
+ }
+
+ // History is returned from newest to oldest preference
+ List<RidePreference> get preferencesHistory =>
+_pastPreferences.reversed.toList();
+}
