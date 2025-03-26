8badbbbde559f8f7343e8d2e109af65a514e7dce
KhengChamnan <chamnan.kheng@student.cadt.edu.kh>
2025-03-26 10:14:54 +0700

    use locationdto to convert tojson instead

--- lib/data/dto/ride_preference_dto.dart
+++ lib/data/dto/ride_preference_dto.dart
@@ -2,18 +2,13 @@ import 'package:week_3_blabla_project/data/dto/location_dto.dart';
 import 'package:week_3_blabla_project/model/ride/ride_pref.dart';
 
 class RidePreferenceDto {
+  
   // Static method to convert  model to JSON
   static Map<String, dynamic> toJson(RidePreference model) {
     return {
-      'departure': {
-        'name': model.departure.name,
-        'country': model.departure.country.name,
-      },
+      'departure': LocationDto.toJson(model.departure),
       'departureDate': model.departureDate.toIso8601String(),
-      'arrival': {
-        'name': model.arrival.name,
-        'country': model.arrival.country.name,
-      },
+      'arrival': LocationDto.toJson(model.arrival),
       'requestedSeats': model.requestedSeats,
     };
   }
